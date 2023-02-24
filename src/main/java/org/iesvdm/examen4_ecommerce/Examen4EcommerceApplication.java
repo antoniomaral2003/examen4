d8a10b7cc750be2e1b3976c8939f658fa59cc83b
antoniomartin <amarala275@g.educaand.es>
2023-02-24 10:51:12 +0100

    Examen 4

---
+++ src/main/java/org/iesvdm/examen4_ecommerce/Examen4EcommerceApplication.java
@@ -0,0 +1,13 @@
+package org.iesvdm.examen4_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen4EcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Examen4EcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/iesvdm/examen4_ecommerce/Examen4EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesvdm.examen4_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen4EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
